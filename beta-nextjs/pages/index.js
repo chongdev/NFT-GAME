185c59124cae162eb263092fe022e681d9f03fbc
Puchong Suanjang <monkey.d.chong@gmail.com>
2022-02-17 20:39:46 +0700

    update: demo vue3

---
+++ beta-nextjs/pages/index.js
@@ -0,0 +1,21 @@
+function HomePage() {
+  return (
+    <>
+
+      <div className="homepage_HeaderSection_156Aj">
+        <div className="homepage_BackgroundVideoContainer_33v9_">
+          <video autoPlay={true} preload="auto" loop={true} playsInline={true} poster={`/images/backgrounds/bg_slilder-11.jpg`}>
+            {/* <source
+              type="video/mp4"
+              src={`https://kr.ncsoft.com/_resource/whatWeCreate/video/lineage2.mp4`}
+            /> */}
+          </video>
+        </div>
+      </div>
+
+      {/* <div className="homepage_ChooseSection_1P0s6"></div> */}
+    </>
+  );
+}
+
+export default HomePage;
---
+++ next-bootstrap5-2/next.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  reactStrictMode: true,
+  ssr: false
+}
